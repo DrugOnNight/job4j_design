f835e51a5186f1cd9009feeadf8a04f63b7483f9
olegg <oleg.god.2013@mail.ru>
2022-06-07 20:49:49 +0300

    0. Что такое обобщенные типы (generics) [#4952]

---
+++ src/main/java/ru/job4j/generics/Predator.java
@@ -0,0 +1,13 @@
+package ru.job4j.generics;
+
+public class Predator extends Animal {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ru/job4j/generics/Tiger.java
@@ -0,0 +1,13 @@
+package ru.job4j.generics;
+
+public class Tiger extends Predator {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
