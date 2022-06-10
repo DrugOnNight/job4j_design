7b24f1ab1d076f65c468c9714985f95befe5326d
olegg <oleg.god.2013@mail.ru>
2022-06-10 11:18:04 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -3,6 +3,7 @@ package ru.job4j.map;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -15,6 +16,11 @@ public class User {
         this.birthday = birthday;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
     public static void main(String[] args) {
         User user1 = new User("Alla", 1,
                 new Calendar.Builder().setTimeOfDay(10, 11, 12, 13).build());
@@ -24,6 +30,9 @@ public class User {
         Map<User, Object> map = new HashMap<>(capacity);
         map.put(user1, new Object());
         map.put(user2, new Object());
+        System.out.println("Hashcode объекта 1: " + user1.hashCode());
+        System.out.println("Hashcode объекта 2: " + user2.hashCode());
+        System.out.println();
         System.out.println("Номер бакета для объекта 1: " + (user1.hashCode() & (capacity - 1)));
         System.out.println("Номер бакета для объекта 2: " + (user2.hashCode() & (capacity - 1)));
         System.out.println(map);
