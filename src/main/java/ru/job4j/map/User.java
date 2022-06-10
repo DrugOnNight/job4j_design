f99f9f9da04b6184b443044e0cb57c5b0863045b
olegg <oleg.god.2013@mail.ru>
2022-06-10 11:02:22 +0300

    2. Без переопределения equals и hashCode [#1005 #310959]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +13,19 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Alla", 1,
+                new Calendar.Builder().setTimeOfDay(10, 11, 12, 13).build());
+        User user2 = new User("Alla", 1,
+                new Calendar.Builder().setTimeOfDay(10, 11, 12, 13).build());
+        int capacity = 16;
+        Map<User, Object> map = new HashMap<>(capacity);
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println("Номер бакета для объекта 1: " + (user1.hashCode() & (capacity - 1)));
+        System.out.println("Номер бакета для объекта 2: " + (user2.hashCode() & (capacity - 1)));
+        System.out.println(map);
     }
 }
