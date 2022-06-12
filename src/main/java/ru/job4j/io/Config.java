8c8fee81e9e21e660a7fb3561f7fd00f091c7bf1
olegg <oleg.god.2013@mail.ru>
2022-06-12 17:29:29 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            String line;
+            String[] splitLine;
+            while ((line = read.readLine()) != null) {
+                if (line.matches("^.[^#].+=.+$")) {
+                    splitLine = line.split("=", 2);
+                    values.put(splitLine[0], splitLine[1]);
+                } else if (!line.matches("^\\s*#.*$") && !line.matches("^$")) {
+                    throw new IllegalArgumentException();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        Config cfg = new Config("app.properties");
+        System.out.println(cfg);
+        cfg.load();
+        System.out.println(cfg.values);
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,76 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+
+    @Test
+    public void configTest() {
+        String path = "app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("name"), "Oleg");
+        assertEquals(config.value("surname"), "Godizov");
+        assertNull(config.value("age"));
+    }
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("name"), "Petr Arsentev");
+        assertNull(config.value("surname"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("name"), "Oleg");
+        assertEquals(config.value("age"), "22");
+    }
+
+    @Test
+    public void whenPairWithEmptyLine() {
+        String path = "./data/pair_with_empty_Line.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("name"), "Oleg");
+        assertEquals(config.value("age"), "22");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenKeyOnly() {
+        String path = "./data/key_only.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("name"), "Oleg");
+        assertEquals(config.value("age"), "22");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenValueOnly() {
+        String path = "./data/value_only.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("name"), "Oleg");
+        assertEquals(config.value("age"), "22");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenEqualsSymbolOnly() {
+        String path = "./data/equals_symbol_only.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenWithoutEqualsSymbol() {
+        String path = "./data/without_equals_symbol.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+}
