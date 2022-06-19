e33581b83ff3e333d38d3e15bb512224e3802d5e
olegg <oleg.god.2013@mail.ru>
2022-06-19 19:36:19 +0300

    5. Валидация параметров запуска. [#246865 #313964]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,14 +12,21 @@ public class Search {
     public static void main(String[] args) throws IOException {
         validate(args);
         Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith("." + args[1]))
+        search(start, p -> p.toFile().getName().endsWith(args[1]))
                 .forEach(System.out::println);
     }
 
     public static void validate(String[] args) {
         if (args.length != 2) {
-            throw new IllegalArgumentException("Use 2 params: "
-                    + "java -jar Search.jar ROOT_FOLDER FILE_EXTENSION");
+            throw new IllegalArgumentException("Use 2 params:"
+                    + " java -jar Search.jar ROOT_FOLDER FILE_EXTENSION");
+        }
+        File firstArg = new File(args[0]);
+        if (!firstArg.exists() || !firstArg.isDirectory()) {
+            throw new IllegalArgumentException("First argument must be an existing directory");
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("File extension must start with a dot.");
         }
     }
 
