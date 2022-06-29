831b3998c99f6c22f7626997abf6d591919d70dd
olegg <oleg.god.2013@mail.ru>
2022-06-29 23:40:50 +0300

    0. Что такое база данных. [#6860]

--- script.sql
+++ script.sql
@@ -1,1 +1,18 @@
+CREATE TABLE books(
+    id serial primary key,
+    title varchar(255),
+    price money,
+    stock integer
+);
+
+INSERT INTO books(title, price, stock) values('Clean code', 2500, 28);
+
+SELECT * FROM books;
+
+UPDATE books SET stock=27;
+
+SELECT * FROM books;
+
+DELETE FROM books;
+
 SELECT * FROM books;
