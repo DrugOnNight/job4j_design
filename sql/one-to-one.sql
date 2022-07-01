1fb35d3331f98772ae85080d7b473fb9ea8df5df
olegg <oleg.god.2013@mail.ru>
2022-07-01 08:46:35 +0300

    0.1. Связи между таблицами [#373615 #317393]

--- sql/one-to-one.sql
+++ sql/one-to-one.sql
@@ -1,15 +1,15 @@
-CREATE TABLE authors(
+CREATE TABLE author(
     id serial primary key,
     name varchar(255)
 );
 
-CREATE TABLE phones(
+CREATE TABLE phone(
     id serial primary key,
     phone text,
 );
 
 CREATE TABLE author_phone(
     id serial primary key,
-    author_id int references authors(id),
-    phone_id int references phones(id)
+    author_id int references author(id) unique,
+    phone_id int references phone(id) unique
 );
